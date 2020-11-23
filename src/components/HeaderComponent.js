7ff2dd349466166bb8fcc8730170120f3da748d4
Aravinda Bose <aravindabose06@gmail.com>
2020-11-23 23:53:42 +0530

    fluid jumbotron and working hamburger menu button

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -9,17 +9,17 @@ function HeaderComponent({ isCartShown, toggleCart, noOfItems }) {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="nav navbar-nav mr-auto">
                         <li className="nav-item active">
                             <a style={{cursor: "pointer"}} onClick={() => toggleCart(false)} className="nav-link">Home</a>
                         </li>
                     </ul>
-                    <a onClick={() => toggleCart()} className="btn btn-outline-secondary text-white"> { isCartShown ? (<>Go Back</>) : (<>Go to Cart <span className="badge badge-danger">{noOfItems}</span></>) }</a>
+                    <a style={{cursor: "pointer"}} onClick={() => toggleCart()} className="nav-link pl-0 text-white"> { isCartShown ? (<>Go Back</>) : (<>Go to Cart <span className="badge badge-danger">{noOfItems}</span></>) }</a>
                 </div>
             </nav>
             { 
                 isCartShown ? null : 
-                (<div className="jumbotron bg-secondary text-white mb-4">
+                (<div className="jumbotron jumbotron-fluid bg-secondary text-white mb-4">
                     <div className="container-fluid">
                         <div className="">
                             <div className="col-12 col-sm-6">
